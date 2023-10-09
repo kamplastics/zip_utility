bca945680d7e40119a3f22e4579e4272c96b86c2
Daniel Van Den Bosch <dvandenbosch@kamplastics.com>
2023-10-09 10:40:51 -0400

    initial commit

---
+++ readme.md
@@ -0,0 +1,56 @@
+# ZipUtility
+
+ZipUtility is a command-line tool developed in C# for extracting ZIP files from a source directory to a destination directory.
+
+## Prerequisites
+
+- .NET Framework (Version compatible with C# 5.0)
+
+## Installation
+
+1. Clone the repository or download the source code.
+2. Compile the code using a C# compiler or an IDE like Visual Studio.
+3. Obtain the executable (`ZipUtility.exe`) from the build output.
+
+## Usage
+
+Run the utility from the command line using the following format:
+
+ZipUtility.exe --from "path_to_zip_files" --to "destination_path"
+
+sql
+Copy code
+
+### Arguments:
+
+- `--from`: Specifies the path to the directory containing ZIP files.
+- `--to`: Specifies the path to the destination directory where the files will be extracted.
+
+For help or to view the usage guidelines, use:
+
+ZipUtility.exe --help
+
+r
+Copy code
+
+### Example:
+
+If you have a directory `C:\zippedFiles` containing ZIP files and you want to extract them to `C:\extractedFiles`, you would use:
+
+ZipUtility.exe --from "C:\zippedFiles" --to "C:\extractedFiles"
+
+markdown
+Copy code
+
+## Error Handling
+
+The utility will provide feedback in the following scenarios:
+
+- Missing `--from` or `--to` arguments.
+- Either the source or the destination directory does not exist.
+- No ZIP files found in the source directory.
+- An error occurs during the extraction of a specific ZIP file.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
---
+++ zip_extractor.cs
@@ -0,0 +1,79 @@
+using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace ZipUtility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0 || Array.Exists(args, arg => arg.Equals("--help", StringComparison.OrdinalIgnoreCase)))
+            {
+                DisplayHelp();
+                return;
+            }
+
+            string fromDirectory = null;
+            string toDirectory = null;
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].Equals("--from", StringComparison.OrdinalIgnoreCase) && i + 1 < args.Length)
+                {
+                    fromDirectory = args[++i];
+                }
+                else if (args[i].Equals("--to", StringComparison.OrdinalIgnoreCase) && i + 1 < args.Length)
+                {
+                    toDirectory = args[++i];
+                }
+            }
+
+            if (string.IsNullOrEmpty(fromDirectory) || string.IsNullOrEmpty(toDirectory))
+            {
+                Console.WriteLine("Error: Missing --from or --to argument");
+                DisplayHelp();
+                return;
+            }
+
+            if (!Directory.Exists(fromDirectory) || !Directory.Exists(toDirectory))
+            {
+                Console.WriteLine("Error: Either the source or the destination directory does not exist.");
+                return;
+            }
+
+            string[] zipFiles = Directory.GetFiles(fromDirectory, "*.zip");
+            if (zipFiles.Length == 0)
+            {
+                Console.WriteLine("No ZIP files found in the source directory.");
+                return;
+            }
+
+            foreach (string zipFile in zipFiles)
+            {
+                string destination = Path.Combine(toDirectory, Path.GetFileNameWithoutExtension(zipFile));
+
+                try
+                {
+                    Console.WriteLine("Extracting {0} to {1}...", zipFile, destination);
+                    ZipFile.ExtractToDirectory(zipFile, destination);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error extracting {0}: {1}", zipFile, ex.Message);
+                }
+            }
+
+            Console.WriteLine("Extraction completed.");
+        }
+
+        static void DisplayHelp()
+        {
+            Console.WriteLine("ZipUtility Help:");
+            Console.WriteLine("Usage: UtilityName.exe --from \"path_to_zip_files\" --to \"destination_path\"");
+            Console.WriteLine("Arguments:");
+            Console.WriteLine("  --from\tPath to the directory containing zip files.");
+            Console.WriteLine("  --to\t\tPath to the destination directory where files will be extracted.");
+        }
+    }
+}
